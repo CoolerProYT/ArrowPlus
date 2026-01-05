b178a2e838518d788b5fc572ce90b3ab28487e1c
CoolerProYT <veronlam1818@gmail.com>
2026-01-05 14:07:30 +0800

    3.2.0

--- src/main/java/com/coolerpromc/arrowplus/recipe/ArrowRecipe.java
+++ src/main/java/com/coolerpromc/arrowplus/recipe/ArrowRecipe.java
@@ -11,7 +11,6 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.CraftingBookCategory;
 import net.minecraft.world.item.crafting.CraftingInput;
 import net.minecraft.world.item.crafting.CustomRecipe;
@@ -30,55 +29,18 @@ public class ArrowRecipe extends CustomRecipe {
     @Override
     public boolean matches(CraftingInput craftingInput, Level level) {
         if (craftingInput.width() == 1 && craftingInput.height() == 3 && craftingInput.ingredientCount() == 3){
-            List<Either<Holder<Item>, TagKey<Item>>> materialList = new ArrayList<>();
-
             List<Holder.Reference<ArrowData>> arrowDataList = level.registryAccess().lookupOrThrow(ModRegistries.ARROW_DATA_KEY).listElements().filter(reference -> !ArrowPlusConfig.CONFIG.getRemoval().contains(reference.key().location().getPath())).toList();
-            arrowDataList.forEach(arrowData -> materialList.add(arrowData.value().material()));
-
-            boolean hasStick = false;
-            boolean hasFeather = false;
-
-            Item materialItem = null;
-            TagKey<Item> materialTag = null;
 
             ItemStack materialStack = craftingInput.getItem(0);
+            ItemStack stickStack = craftingInput.getItem(1);
+            ItemStack featherStack = craftingInput.getItem(2);
 
-            for (Either<Holder<Item>, TagKey<Item>> material : materialList){
-                if (material.left().isPresent() && materialStack.is(material.left().get())) {
-                    materialItem = materialStack.getItem();
-                    break;
-                } else if (material.right().isPresent() && materialStack.is(material.right().get())) {
-                    materialTag = material.right().get();
-                    break;
-                }
-            }
-
-            Item stick = Items.AIR;
-            Item feather = Items.AIR;
             for (Holder<ArrowData> arrowData : arrowDataList){
-                if (materialItem != null && arrowData.value().material().left().orElseThrow().is(materialItem.builtInRegistryHolder())){
-                    stick = arrowData.value().stick().value();
-                    feather = arrowData.value().feather().value();
-                    break;
-                }
-                else if (materialTag != null && arrowData.value().material().right().orElseThrow().equals(materialTag)){
-                    stick = arrowData.value().stick().value();
-                    feather = arrowData.value().feather().value();
-                    break;
+                if (arrowData.value().isValidMaterial(materialStack, stickStack, featherStack)){
+                    return true;
                 }
             }
-
-            if (stick == Items.AIR || feather == Items.AIR) return false;
-
-            if (craftingInput.getItem(1).is(stick)){
-                hasStick = true;
-            }
-
-            if (craftingInput.getItem(2).is(feather)){
-                hasFeather = true;
-            }
-
-            return hasStick && hasFeather;
+            return false;
         }
         else {
             return false;
