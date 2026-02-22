395d484e8a031f5b8affb3bb3d66444a17e3d12e
CoolerProYT <veronlam1818@gmail.com>
2026-02-22 14:16:09 +0800

    3.3.1
    3.3.1

--- src/main/java/com/coolerpromc/arrowplus/ArrowDataGenerator.java
+++ src/main/java/com/coolerpromc/arrowplus/ArrowDataGenerator.java
@@ -1,23 +1,21 @@
 package com.coolerpromc.arrowplus;
 
+import com.coolerpromc.arrowplus.arrow.Arrows;
 import com.coolerpromc.arrowplus.datagen.ModDatapackProvider;
 import com.coolerpromc.arrowplus.datagen.ModItemTagsProvider;
 import com.coolerpromc.arrowplus.datagen.ModModelProvider;
 import com.coolerpromc.arrowplus.datagen.ModRecipeProvider;
-import com.coolerpromc.arrowplus.arrow.Arrows;
 import com.coolerpromc.arrowplus.registry.ModRegistries;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.minecraft.registry.RegistryBuilder;
-import net.minecraft.registry.RegistryWrapper;
-
-import java.util.concurrent.CompletableFuture;
 
 public class ArrowDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
-		CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture = fabricDataGenerator.getRegistries();
+
+		if (!(fabricDataGenerator.getModId().equals(ArrowPlus.MODID))) return;
 
 		pack.addProvider(ModModelProvider::new);
 		pack.addProvider(ModItemTagsProvider::new);
