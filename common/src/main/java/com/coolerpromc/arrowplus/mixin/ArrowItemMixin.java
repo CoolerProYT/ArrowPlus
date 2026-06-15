eca2dff5b8e62986c922943efe5cfde420551a2b
CoolerProYT <veronlam1818@gmail.com>
2026-06-15 20:57:21 +0800

    26.1.2.101

--- common/src/main/java/com/coolerpromc/arrowplus/mixin/ArrowItemMixin.java
+++ common/src/main/java/com/coolerpromc/arrowplus/mixin/ArrowItemMixin.java
@@ -22,15 +22,17 @@ public class ArrowItemMixin extends Item {
         Window handle = Minecraft.getInstance().getWindow();
         boolean shiftDown = InputConstants.isKeyDown(handle, GLFW.GLFW_KEY_LEFT_SHIFT) || InputConstants.isKeyDown(handle, GLFW.GLFW_KEY_RIGHT_SHIFT);
 
-        if(!shiftDown){
-            tooltips.accept(Component.literal("Hold §8[Shift]§r for more info."));
-        }
-        else{
-            boolean infinity = this.asItem() != Items.SPECTRAL_ARROW && this.asItem() != Items.TIPPED_ARROW;
-            tooltips.accept(Component.translatable("tooltip.arrowplus.base_damage", "§a2.0"));
-            tooltips.accept(Component.translatable("tooltip.arrowplus.flame", "§afalse"));
-            tooltips.accept(Component.translatable("tooltip.arrowplus.gravity", "§a0.05"));
-            tooltips.accept(Component.translatable("tooltip.arrowplus.infinity", "§a" + infinity));
+        if (this == Items.SPECTRAL_ARROW || this == Items.ARROW || this == Items.TIPPED_ARROW){
+            if(!shiftDown){
+                tooltips.accept(Component.literal("Hold §8[Shift]§r for more info."));
+            }
+            else{
+                boolean infinity = this.asItem() != Items.SPECTRAL_ARROW && this.asItem() != Items.TIPPED_ARROW;
+                tooltips.accept(Component.translatable("tooltip.arrowplus.base_damage", "§a2.0"));
+                tooltips.accept(Component.translatable("tooltip.arrowplus.flame", "§afalse"));
+                tooltips.accept(Component.translatable("tooltip.arrowplus.gravity", "§a0.05"));
+                tooltips.accept(Component.translatable("tooltip.arrowplus.infinity", "§a" + infinity));
+            }
         }
     }
 }
